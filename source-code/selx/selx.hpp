97ffde906dcdb91dc0b8d30fa57e0b56ef6c99be
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-07-24 03:02:00 -0300

    Feat: Added "shortcut" header that defaults to the best IO mechanism available

---
+++ source-code/selx/selx.hpp
@@ -0,0 +1,13 @@
+#if defined(_WIN32) || defined(_WIN64)
+ 	#include "iocp.hpp"
+
+	 namespace selx {
+		 using namespace selx::iocp;
+	 }
+#elif defined(unix) || defined (__unix) || defined(__unix__)
+	#include "epoll.hpp"
+
+	namespace selx {
+		using namespace selx::epoll;
+	}
+#endif
